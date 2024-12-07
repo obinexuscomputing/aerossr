208f402b6ebba04e715e9b8945d7270e5371f24c
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 05:33:53 +0000

    ti

---
+++ dist/cjs/utils/bundler.cjs
@@ -0,0 +1,45 @@
+'use strict';
+
+var fs = require('fs/promises');
+var path = require('path');
+
+async function resolveDependencies(filePath, deps = new Set()) {
+    if (deps.has(filePath))
+        return deps;
+    deps.add(filePath);
+    const content = await fs.readFile(filePath, 'utf-8');
+    const importMatches = content.match(/(?:require|import)\s*\(['"]([^'"]+)['"]\)/g);
+    if (importMatches) {
+        for (const match of importMatches) {
+            const depPath = match.match(/['"]([^'"]+)['"]/)?.[1];
+            if (depPath) {
+                const fullPath = path.resolve(path.dirname(filePath), depPath);
+                if (fullPath.endsWith('.js') || fullPath.endsWith('.ts')) {
+                    await resolveDependencies(fullPath, deps);
+                }
+            }
+        }
+    }
+    return deps;
+}
+function minifyBundle(code) {
+    return code
+        .replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '') // Remove comments
+        .replace(/\s+/g, ' ') // Reduce multiple spaces to single space
+        .replace(/^\s+|\s+$/gm, ''); // Trim line starts and ends
+}
+async function generateBundle(projectPath, entryPoint) {
+    const entryFilePath = path.join(projectPath, entryPoint);
+    const dependencies = await resolveDependencies(entryFilePath);
+    let bundle = '';
+    for (const dep of dependencies) {
+        const content = await fs.readFile(dep, 'utf-8');
+        bundle += `\n// File: ${path.relative(projectPath, dep)}\n${content}\n`;
+    }
+    return minifyBundle(bundle);
+}
+
+exports.generateBundle = generateBundle;
+exports.minifyBundle = minifyBundle;
+exports.resolveDependencies = resolveDependencies;
+//# sourceMappingURL=bundler.cjs.map
---
+++ dist/esm/types/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="types/index.d.ts" />
+export * from './types/index';
+export * from './types';
+export * from './middleware/index';
+export * from './utils/';
+//# sourceMappingURL=index.d.ts.map
