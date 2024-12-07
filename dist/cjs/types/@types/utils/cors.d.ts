eaaa320836023f7ca021585612721325c85b436a
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 05:47:23 +0000

    int

--- dist/cjs/types/@types/utils/cors.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { ServerResponse } from 'http';
-export declare function setCorsHeaders(res: ServerResponse, origins?: string): void;
-//# sourceMappingURL=cors.d.ts.map
