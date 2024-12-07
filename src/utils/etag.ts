98f9ca4b2ac47b4af2d9445ceff98a7909bac78c
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 11:09:06 +0000

    it

--- src/utils/etag.ts
+++ src/utils/etag.ts
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import * as crypto from 'crypto';
 
 export function generateETag(content: string | Buffer): string {
   return crypto.createHash('md5').update(content).digest('hex');
