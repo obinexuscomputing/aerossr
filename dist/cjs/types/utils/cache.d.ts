701694aae62c59b060b237707cedf39aee9b06aa
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 13:40:01 +0000

    init

--- dist/cjs/types/utils/cache.d.ts
+++
@@ -1,3 +0,0 @@
-import { CacheStore } from "../types/index";
-export declare function createCache<T>(): CacheStore<T>;
-//# sourceMappingURL=cache.d.ts.map
--- dist/esm/types/utils/etag.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-/// <reference types="node" />
-export declare function generateETag(content: string | Buffer): string;
-//# sourceMappingURL=etag.d.ts.map
