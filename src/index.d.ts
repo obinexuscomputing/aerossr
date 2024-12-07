82768e0a90c991a0e3f8c0c6408c15a1f7e74caf
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 15:23:34 +0000

    it

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,10 @@
-import { IncomingMessage, Server, ServerResponse } from 'http';
-import { AeroSSRConfig, StaticFileOptions, LoggerOptions, CacheStore, MetaTags } from './@types';
-
-export type RouteHandler = (
-    req: IncomingMessage,
-    res: ServerResponse
-) => Promise<void> | void;
-
-export type Middleware = (
-    req: IncomingMessage,
-    res: ServerResponse,
-    next: () => Promise<void>
-) => Promise<void>;
-
-declare namespace AeroSSR {
+export declare namespace AeroSSR {
     class Core {
         constructor(config?: AeroSSRConfig);
         start(): Promise<Server>;
         stop(): Promise<void>;
         use(middleware: Middleware): void;
         route(path: string, handler: RouteHandler): void;
-        readonly config: Required<AeroSSRConfig>;
     }
 
     namespace Middleware {
@@ -33,40 +18,30 @@ declare namespace AeroSSR {
         class Logger {
             constructor(options?: LoggerOptions);
             log(message: string): void;
-            logRequest(req: IncomingMessage): void;
         }
-
+        
         namespace Cache {
             function create<T>(): CacheStore<T>;
         }
-
+        
         namespace HTTP {
             function setCorsHeaders(res: ServerResponse, origins?: string): void;
             function generateETag(content: string | Buffer): string;
         }
-
+        
         namespace Error {
             function generatePage(statusCode: number, message: string): string;
-            function handle(
-                error: Error & { statusCode?: number },
-                req: IncomingMessage,
-                res: ServerResponse
-            ): Promise<void>;
+            function handle(error: Error & { statusCode?: number }, req: IncomingMessage, res: ServerResponse): Promise<void>;
         }
-
+        
         namespace HTML {
-            function injectMetaTags(
-                html: string,
-                meta?: MetaTags,
-                defaultMeta?: MetaTags
-            ): string;
+            function injectMetaTags(html: string, meta?: MetaTags, defaultMeta?: MetaTags): string;
         }
-
+        
         namespace Bundle {
             function generate(projectPath: string, entryPoint: string): Promise<string>;
         }
     }
 }
 
-export { AeroSSR };
-export default AeroSSR.Core;
+export = AeroSSR;
