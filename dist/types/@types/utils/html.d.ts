9de749e18eed6b0c0db5ddb09c7a7eabbe18e7e5
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 13:55:42 +0000

    it

--- dist/types/@types/utils/html.d.ts
+++
@@ -1,9 +0,0 @@
-export interface MetaTags {
-    charset?: string;
-    viewport?: string;
-    description?: string;
-    title?: string;
-    [key: string]: string | undefined;
-}
-export declare function injectMetaTags(html: string, meta?: MetaTags, defaultMeta?: MetaTags): string;
-//# sourceMappingURL=html.d.ts.map
