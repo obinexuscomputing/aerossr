2e31f6b3ec42cc5b5bfc75292929208c4e0db5ec
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-01 13:57:18 +0000

    it

--- dist/cjs/_virtual/4fc01db43b6bcbc543d2c66036d6a6.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const __dirname$1 = '/Users/OBINexusComputing/Projects/obinexuscomputing/pkg/aerossr';
-
-exports.default = __dirname$1;
-//# sourceMappingURL=4fc01db43b6bcbc543d2c66036d6a6.cjs.map
--- dist/esm/_virtual/03b65a12d71180df8f9a9741b2d186.js
+++
@@ -1,4 +0,0 @@
-const __dirname = '/Users/OBINexusComputing/Projects/obinexuscomputing/pkg/aerossr';
-
-export { __dirname as default };
-//# sourceMappingURL=03b65a12d71180df8f9a9741b2d186.js.map
