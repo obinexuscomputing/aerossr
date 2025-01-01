485f75da397d878869fe2382f3aaf0b2c111df20
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-01 23:29:15 +0000

    itg

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
-import dts from 'rollup-plugin-dts';
+import * as dts from 'rollup-plugin-dts';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import alias from '@rollup/plugin-alias';
-import { resolve as resolvePath } from 'path';
+import { resolve as _resolve } from 'path';
 import { readFileSync } from 'fs';
 
 const pkg = JSON.parse(readFileSync('./package.json'));
@@ -38,18 +38,22 @@ const external = [
 // Alias configuration for path resolution
 const aliasEntries = {
   entries: [
-    { find: /^@\/(.*)/, replacement: resolvePath(process.cwd(), 'src/$1') },
-    { find: /^@utils\/(.*)/, replacement: resolvePath(process.cwd(), 'src/utils/$1') },
-    { find: /^@types\/(.*)/, replacement: resolvePath(process.cwd(), 'src/types/$1') },
-    { find: '@', replacement: resolvePath(process.cwd(), 'src') },
-    { find: '@utils', replacement: resolvePath(process.cwd(), 'src/utils') },
-    { find: '@types', replacement: resolvePath(process.cwd(), 'src/types') }
+    { find: /^@\/(.*)/, replacement: _resolve(process.cwd(), 'src/$1') },
+    { find: /^@utils\/(.*)/, replacement: _resolve(process.cwd(), 'src/utils/$1') },
+    { find: /^@types\/(.*)/, replacement: _resolve(process.cwd(), 'src/types/$1') },
+    { find: '@', replacement: _resolve(process.cwd(), 'src') },
+    { find: '@utils', replacement: _resolve(process.cwd(), 'src/utils') },
+    { find: '@types', replacement: _resolve(process.cwd(), 'src/types') }
   ]
 };
 
 // Common plugins used across all builds
 const commonPlugins = [
   alias(aliasEntries),
+  typescript({
+    tsconfig: './tsconfig.json',
+    sourceMap: true
+  }),
   resolve({ 
     preferBuiltins: true,
     extensions: ['.ts', '.js', '.json'],
@@ -63,19 +67,6 @@ const commonPlugins = [
   nodePolyfills(),
   isProduction && terser()
 ].filter(Boolean);
-
-// TypeScript configuration for different build targets
-const createTypescriptPlugin = (outDir, declaration = true, declarationMap = true) => 
-  typescript({
-    tsconfig: './tsconfig.json',
-    outDir,
-    declaration,
-    declarationMap,
-    sourceMap: true,
-    exclude: ['**/__tests__/**'],
-    include: ['src/**/*'],
-    moduleResolution: 'node'
-  });
 
 // Input files for all builds
 const inputFiles = {
@@ -96,7 +87,7 @@ export default [
       preserveModulesRoot: 'src'
     },
     external,
-    plugins: [...commonPlugins, createTypescriptPlugin('dist/esm')]
+    plugins: commonPlugins
   },
 
   // CJS Build
@@ -112,7 +103,7 @@ export default [
       interop: 'auto'
     },
     external,
-    plugins: [...commonPlugins, createTypescriptPlugin('dist/cjs')]
+    plugins: commonPlugins
   },
 
   // Type Definitions
@@ -127,7 +118,7 @@ export default [
     external,
     plugins: [
       alias(aliasEntries),
-      dts({
+      dts.default({
         respectExternal: true,
         compilerOptions: {
           baseUrl: '.',
