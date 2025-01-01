dbe00d7bb1e714108d4bc2976b598ae233aaafd3
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-01 23:45:56 +0000

    added-tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,30 @@
-// jest.config.js
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   testMatch: [
-    "**/__tests__/**/*.test.ts"
+    '**/__tests__/**/*.test.ts',
+    '**/?(*.)+(spec|test).ts'
   ],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         useESM: true,
-      },
-    ],
+        tsconfig: 'tsconfig.test.json'
+      }
+    ]
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1'  // Handle ESM imports
   },
   extensionsToTreatAsEsm: ['.ts'],
   moduleDirectories: ['node_modules', 'src'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
   coverageThreshold: {
     global: {
       branches: 80,
@@ -31,5 +33,14 @@ export default {
       statements: 80
     }
   },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/types/**',
+    '!src/**/index.ts'
+  ],
+  setupFilesAfterEnv: ['./__tests__/setup.ts'],
+  testTimeout: 10000,
+  verbose: true,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
+}
---
+++ tsconfig,test.ts
@@ -0,0 +1,16 @@
+{
+    "extends": "./tsconfig.json",
+    "include": [
+      "src/**/*.ts",
+      "src/**/*.tsx",
+      "__tests__/**/*.ts",
+      "**/*.test.ts",
+      "**/*.spec.ts"
+    ],
+      "exclude": [
+        "node_modules",
+        "dist"
+      ]
+    }
+  }
+}
