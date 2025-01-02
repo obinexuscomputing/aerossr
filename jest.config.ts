96f398b382e11ea25ee72114b84daf07209752ef
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 15:39:01 +0000

    it

--- jest.config.ts
+++ jest.config.ts
@@ -1,47 +1,27 @@
-// jest.config.ts
-export default {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'node', // Change this if your tests require a different environment
   roots: ['<rootDir>/src', '<rootDir>/__tests__'],
-  testMatch: [
-    '**/__tests__/**/*.test.ts',
-    '**/?(*.)+(spec|test).ts'
+  testMatch: ['**/__tests__/**/*.(test|spec).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
   ],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-        tsconfig: 'tsconfig.test.json'
-      }
-    ]
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^@types/(.*)$': '<rootDir>/src/types/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1'  // Handle ESM imports
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleDirectories: ['node_modules', 'src'],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'clover'],
+  coverageDirectory: '<rootDir>/coverage',
   coverageThreshold: {
     global: {
+      statements: 80,
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
+    },
   },
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/types/**',
-    '!src/**/index.ts'
-  ],
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
-  testTimeout: 10000,
-  verbose: true,
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'], // Path to setup file, if needed
 };
+
+export default config;
