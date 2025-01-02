c5287d90c2f5bdf29f47a650ac1c8dde4ea054cc
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 17:43:38 +0000

    it

--- dist/esm/cli/commands.d.ts
+++
@@ -1,9 +0,0 @@
-import { AeroSSR } from '../';
-export interface MiddlewareConfig {
-    name: string;
-    path: string;
-    options?: Record<string, unknown>;
-}
-export declare function initializeSSR(directory: string): Promise<void>;
-export declare function configureMiddleware(app: AeroSSR, config?: MiddlewareConfig): Promise<void>;
-//# sourceMappingURL=commands.d.ts.map
--- dist/esm/types/utils/async.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnyFunction } from "..";
-export declare function isPromise<T = unknown>(value: unknown): value is Promise<T>;
-export declare function ensureAsync<T extends AnyFunction>(fn: T): (...args: Parameters<T>) => Promise<ReturnType<T>>;
-//# sourceMappingURL=async.d.ts.map
