e24c482aeab63cfae8cc6b10bcf56a451d30159f
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 18:21:36 +0000

    itgg

--- dist/cli/bin/middlewares/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './StaticFileMiddleware';
-export * from './SecurityMiddleware';
-//# sourceMappingURL=index.d.ts.map
--- dist/esm/utils/etag.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-/// <reference types="node" />
-export interface ETagOptions {
-    weak?: boolean;
-}
-export declare function generateETag(content: string | Buffer, options?: ETagOptions): string;
-//# sourceMappingURL=etag.d.ts.map
