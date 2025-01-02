f9ffd7870f25449a7957b88e685da3ae4632a4d8
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 18:31:35 +0000

    itgg

--- dist/cli/bin/types/types/utils/async.d.ts
+++ dist/cli/bin/types/types/utils/async.d.ts
@@ -1,10 +1,4 @@
-import type { AnyFunction } from '../types';
-/**
- * Type guard to check if a value is a Promise
- */
+import { AnyFunction } from "..";
 export declare function isPromise<T = unknown>(value: unknown): value is Promise<T>;
-/**
- * Ensures a function returns a Promise
- */
 export declare function ensureAsync<T extends AnyFunction>(fn: T): (...args: Parameters<T>) => Promise<ReturnType<T>>;
 //# sourceMappingURL=async.d.ts.map
