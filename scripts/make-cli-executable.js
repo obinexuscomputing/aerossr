f6b7c721d0754f1a72b1f2ead5cf21451968b48b
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 18:21:23 +0000

    itgg

--- scripts/make-cli-executable.js
+++ scripts/make-cli-executable.js
@@ -1,17 +1,9 @@
-// Import necessary modules
 import { chmodSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join, resolve } from 'path';
+import { join } from 'path';
 
-// Resolve __filename and __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Define the path to the CLI executable
-const cliPath = join(__dirname, '../dist/cli/bin/index.js');
+const cliPath = join(__dirname, '../dist/cli/bin/index.cjs');
 
 try {
-  // Ensure the file exists and make it executable
   chmodSync(cliPath, 0o755);
   console.log(`Made ${cliPath} executable`);
 } catch (err) {
