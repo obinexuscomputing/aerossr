5674bec407e72d395d56ce1c3ffb5018d0423ae8
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-02 15:24:45 +0000

    workigng

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
-import  dts from 'rollup-plugin-dts';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
+import {dts} from 'rollup-plugin-dts';
 import alias from '@rollup/plugin-alias';
 import { resolve as _resolve } from 'path';
 import { readFileSync } from 'fs';
@@ -12,30 +11,6 @@ import { readFileSync } from 'fs';
 const pkg = JSON.parse(readFileSync('./package.json'));
 const isProduction = process.env.NODE_ENV === 'production';
 
-// Include all Node.js built-in modules
-const builtins = [
-  'path', 
-  'fs', 
-  'http', 
-  'crypto', 
-  'zlib', 
-  'url', 
-  'stream', 
-  'os', 
-  'util',
-  'events',
-  'buffer'
-];
-
-// External dependencies that shouldn't be bundled
-const external = [
-  ...builtins,
-  ...Object.keys(pkg.dependencies || {}),
-  ...Object.keys(pkg.peerDependencies || {}),
-  /@types\/.*/,  // Exclude all @types packages
-];
-
-// Alias configuration for path resolution
 const aliasEntries = {
   entries: [
     { find: /^@\/(.*)/, replacement: _resolve(process.cwd(), 'src/$1') },
@@ -47,28 +22,13 @@ const aliasEntries = {
   ]
 };
 
-// Common plugins used across all builds
-const commonPlugins = [
-  alias(aliasEntries),
-  typescript({
-    tsconfig: './tsconfig.json',
-    sourceMap: true
-  }),
-  resolve({ 
-    preferBuiltins: true,
-    extensions: ['.ts', '.js', '.json'],
-    resolveOnly: [/^(?!@types).+/]
-  }),
-  commonjs({
-    include: /node_modules/,
-    extensions: ['.js', '.ts']
-  }),
-  json(),
-  nodePolyfills(),
-  isProduction && terser()
-].filter(Boolean);
+const external = [
+  'path', 'fs', 'http', 'crypto', 'zlib', 'url', 'stream', 'os', 'util', 'events', 'buffer',
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+  /@types\/.*/
+];
 
-// Input files for all builds
 const inputFiles = {
   'index': 'src/index.ts',
   'cli': 'src/cli/index.ts',
@@ -87,7 +47,20 @@ export default [
       preserveModulesRoot: 'src'
     },
     external,
-    plugins: commonPlugins
+    plugins: [
+      alias(aliasEntries),
+      typescript({
+        tsconfig: './tsconfig.json',
+        sourceMap: true
+      }),
+      resolve({ 
+        preferBuiltins: true,
+        extensions: ['.ts', '.js', '.json']
+      }),
+      commonjs(),
+      json(),
+      isProduction && terser()
+    ].filter(Boolean)
   },
 
   // CJS Build
@@ -99,11 +72,23 @@ export default [
       sourcemap: true,
       preserveModules: true,
       preserveModulesRoot: 'src',
-      exports: 'named',
-      interop: 'auto'
+      exports: 'named'
     },
     external,
-    plugins: commonPlugins
+    plugins: [
+      alias(aliasEntries),
+      typescript({
+        tsconfig: './tsconfig.json',
+        sourceMap: true
+      }),
+      resolve({ 
+        preferBuiltins: true,
+        extensions: ['.ts', '.js', '.json']
+      }),
+      commonjs(),
+      json(),
+      isProduction && terser()
+    ].filter(Boolean)
   },
 
   // Type Definitions
@@ -115,20 +100,9 @@ export default [
       preserveModules: true,
       preserveModulesRoot: 'src'
     },
-    external,
     plugins: [
       alias(aliasEntries),
-      dts.default({
-        respectExternal: true,
-        compilerOptions: {
-          baseUrl: '.',
-          paths: {
-            '@/*': ['src/*'],
-            '@utils/*': ['src/utils/*'],
-            '@types/*': ['src/types/*']
-          }
-        }
-      })
+      dts()
     ]
   }
 ];
