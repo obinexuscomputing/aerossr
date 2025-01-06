19cfe12138fd4a5de11dc2926e40fb3b7d7cae70
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-06 17:01:00 +0000

    it

--- jest.config.ts
+++ jest.config.ts
@@ -9,54 +9,43 @@ const config: Config.InitialOptions = {
   roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   
   testMatch: [
-    '**/__tests__/**/*.test.ts',
-    '**/__tests__/**/*.spec.ts'
+    '**/__tests__/**/*.(test|spec).ts',
+    '**/__tests__/**/*.(test|spec).tsx',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
   
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^src/(.*)$': '<rootDir>/src/$1',
-    '\\.txt$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    '^src/(.*)$': '<rootDir>/src/$1'
   },
 
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/coverage/', '/.rollup/', '/buildcache/'],
-  moduleDirectories: ['node_modules', 'src'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node','test'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    '/.rollup/',
+    '/buildcache/'
+  ],
+
+  moduleDirectories: ['node_modules', 'src', '__tests__'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
+    '^.+\\.[tj]sx?$': ['ts-jest', {
       tsconfig: 'tsconfig.test.json',
-      diagnostics: { 
-        warnOnly: true,
-        ignoreCodes: [151001]
-      }
+      diagnostics: { warnOnly: true }
     }]
   },
 
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/types/**',
-    '!**/*.test.ts',
-    '!**/*.spec.ts'
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  },
-
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-
+  
   globals: {
     'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
       isolatedModules: true
     }
   },
@@ -66,28 +55,9 @@ const config: Config.InitialOptions = {
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
-  
-  modulePaths: ['<rootDir>'],
-  rootDir: './',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  resolver: 'ts-jest-resolver',
 
-  // Project-specific settings
-  projects: [
-    {
-      displayName: 'node',
-      testEnvironment: 'node',
-      testMatch: ['**/__tests__/**/*.node.test.ts']
-    },
-    {
-      displayName: 'jsdom',
-      testEnvironment: 'jsdom',
-      testMatch: ['**/__tests__/**/*.dom.test.ts'],
-      testEnvironmentOptions: {
-        url: 'http://localhost'
-      }
-    }
-  ]
+  projects: undefined,
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
 };
 
 export default config;
