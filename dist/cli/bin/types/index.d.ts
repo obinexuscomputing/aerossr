934a1641c6e92c0a5aeca5383f2ffd683a9e6230
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-06 15:50:09 +0000

    it

--- dist/cli/bin/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as AeroSSR } from './AeroSSR';
-export * from './types';
-export * from './utils';
-export * from './middlewares';
-export type { CacheStoreBase as CacheStore, CorsOptionsBase as CorsOptions, MetaTagsBase as MetaTags, LoggerOptionsBase as LoggerOptions } from './types';
-//# sourceMappingURL=index.d.ts.map
