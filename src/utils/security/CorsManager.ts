9aaca73d38534cfc72df008e2bd9221663043a08
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-01-09 15:59:05 +0000

    it

--- src/utils/security/CorsManager.ts
+++ src/utils/security/CorsManager.ts
@@ -1,5 +1,5 @@
 import { ServerResponse } from 'http';
-import { CorsOptionsBase } from '@/types';
+import { CorsOptionsBase } from '@types';
 
 export interface CorsOptions extends CorsOptionsBase {
   origins?: string | string[];
